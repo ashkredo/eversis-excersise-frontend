45fc73f5ae8a4cf732e1c945ce1676c3b5bf9ec3
Artur Shkred <s15444@pjwstk.edu.pl>
2020-08-21 20:13:54 +0200

    Configured React Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from 'screens/pages/Home';
 
 const App = () => {
-  return <div className="App">Eversis Excersise Frontend</div>;
+  return (
+    <div className="App">
+      <Switch>
+        <Route path="/" exact render={() => <Home />} />
+        <Route path="/home" exact render={() => <Home />} />
+        <Route render={() => <h1>NotFound</h1>} />
+      </Switch>
+    </div>
+  );
 };
 
-export default App;
+const EversisExcersiseApp = (props) => {
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+};
+
+export default EversisExcersiseApp;
